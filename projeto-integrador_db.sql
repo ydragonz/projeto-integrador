eec16b40c8e4f33d643ec6da79706d111d74359a
Leonardo Oliveira <ninjamaxter@gmail.com>
2021-10-26 00:48:43 -0300

    teste

---
+++ projeto-integrador_db.sql
@@ -0,0 +1,36 @@
+DROP SCHEMA
+
+IF EXISTS pi;
+    CREATE SCHEMA pi COLLATE = utf8_general_ci;
+USE pi;
+
+CREATE TABLE curso (
+    id_curso INT() UNSIGNED NOT NULL;  
+    nom_curso VARCHAR(30);
+    PRIMARY KEY(id_curso);
+)
+
+CREATE TABLE usuario (
+    cod_usuario INT() UNSIGNED NOT NULL;
+    id_curso INT();
+    nom_usuario VARCHAR(40);
+    dtn_usuario DATE();
+    sen_usuario VARCHAR(10);
+    per_usuario INT();
+    sts_usuario BOOL;
+    PRIMARY KEY(cod_usuario);
+)
+
+CREATE TABLE paciente (
+    cod_paciente INT() UNSIGNED NOT NULL;
+    nom_paciente VARCHAR(40);
+    sex_paciente CHAR(1);
+
+    PRIMARY KEY(cod_paciente);
+)
+
+
+CREATE TABLE exame (
+    num_exame INT() UNSIGNED NOT NULL;
+    cod_paciente
+)
