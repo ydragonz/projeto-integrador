42a6060af9d5a2ab822d002a9c0cdec58b3f9ebb
Leonardo Oliveira <ninjamaxter@gmail.com>
2021-11-08 15:19:50 -0300

    Index structure update

--- index.php
+++ index.php
@@ -5,5 +5,25 @@
 // -Leonardo Bernardes de Oliveira  
 // -Sara Ferreira Fernandes  
 // -João Carneiro da Cunha  
+
+    if(isset($_GET['p'])) {
+        $arquivo = $_GET['p'];    
+    }
+    else {
+        $arquivo = 'home.php';
+    }
 ?>
 
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Site desenvolvido para o Projeto Integrador da terceira etapa de Engenharia de Software da UNAERP de 2021/2">
+        <meta name="author" content="Leonardo Bernardes de Oliveira, Sara Ferreira Fernandes e João Carneiro da Cunha">
+        <title>Bem-Estar no Shopping</title>
+    </head>
+    <body>
+
+    </body>
+</html>
