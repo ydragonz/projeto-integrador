d97373894dd54f9c5c7e255ac2b5417d305f6aab
Leonardo Oliveira <64879628+ydragonz@users.noreply.github.com>
2021-11-09 19:36:05 -0300

    General update

--- index.php
+++ index.php
@@ -21,9 +21,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="Site desenvolvido para o Projeto Integrador da terceira etapa de Engenharia de Software da UNAERP de 2021/2">
         <meta name="author" content="Leonardo Bernardes de Oliveira, Sara Ferreira Fernandes e João Carneiro da Cunha">
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
         <title>Bem-Estar no Shopping</title>
     </head>
     <body>
-
+    
     </body>
 </html>
