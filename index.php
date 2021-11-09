c5c60711a428b4d0175769925568c087717b49fb
Leonardo Oliveira <64879628+ydragonz@users.noreply.github.com>
2021-11-09 19:37:51 -0300

    General updates

--- index.php
+++ index.php
@@ -25,6 +25,22 @@
         <title>Bem-Estar no Shopping</title>
     </head>
     <body>
-    
+
+    <div class="container-fluid">
+        <div class="row">
+            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+            <?php 
+                include('menu.php');
+            ?>
+            </nav>
+
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <?php
+                include($arquivo);
+            ?>
+
+            </main>
+        </div>
+    </div>
     </body>
 </html>
