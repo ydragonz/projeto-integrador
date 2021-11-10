9bf574a4f01eae38c7c8c104f4f78b40371bb376
Leonardo Oliveira <64879628+ydragonz@users.noreply.github.com>
2021-11-09 21:41:42 -0300

    Bootstrap add

--- index.php
+++ index.php
@@ -21,20 +21,20 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="Site desenvolvido para o Projeto Integrador da terceira etapa de Engenharia de Software da UNAERP de 2021/2">
         <meta name="author" content="Leonardo Bernardes de Oliveira, Sara Ferreira Fernandes e João Carneiro da Cunha">
-        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <title>Bem-Estar no Shopping</title>
     </head>
     <body>
 
     <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Unaerp 2021</a>
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#"><img src="images/unaerp_extended.png" class="img-fluid"></a>
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     
     <div class="navbar-nav">
         <div class="nav-item text-nowrap">
-        <a class="nav-link px-3" href="#">Sign out</a>
+        <a class="nav-link px-3" href="#">Sair</a>
         </div>
     </div>
     </header>
