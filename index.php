8b90441d2d562bb7ad93164958d9685d7db72e67
Leonardo Oliveira <64879628+ydragonz@users.noreply.github.com>
2021-11-09 22:18:25 -0300

    Update

--- index.php
+++ index.php
@@ -22,12 +22,13 @@
         <meta name="description" content="Site desenvolvido para o Projeto Integrador da terceira etapa de Engenharia de Software da UNAERP de 2021/2">
         <meta name="author" content="Leonardo Bernardes de Oliveira, Sara Ferreira Fernandes e João Carneiro da Cunha">
         <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
+        <link rel="icon" href="images/unaerp_logo.jpg">
         <title>Bem-Estar no Shopping</title>
     </head>
     <body>
 
     <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-md-3 col-lg-1 me-0 px-3" href="#"><img src="images/unaerp_extended.png" class="img-fluid" style="max-heigh="></a>
+    <a class="navbar-brand col-md-3 col-lg-1 me-0 px-3" href="#">Bem-Estar</a>
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
