dbb5695ba9f005d00bade76964e98afa1acb7974
Leonardo Oliveira <ninjamaxter@gmail.com>
2021-11-11 00:06:25 -0300

    update

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <h1 class="display-1">Introdução</h1>
 <br>
-<p2 class="fs-4">   O evento Bem-Estar no Shopping é realizado seis vezes ao ano em um Shopping Center 
+<p class="fs-4">O evento Bem-Estar no Shopping é realizado seis vezes ao ano em um Shopping Center 
 do interior paulista, sendo realizados exames de glicemia, colesterol e aferição da 
-pressão arterial, assim como a coleta de dados referentes à saúde dos participantes.</p1>
+pressão arterial, assim como a coleta de dados referentes à saúde dos participantes.</p>
