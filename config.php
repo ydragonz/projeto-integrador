ee1aa11ea6660f1d5bdf7fda826b4a5658ce8752
Leonardo Oliveira <64879628+ydragonz@users.noreply.github.com>
2021-11-12 19:41:28 -0300

    Connection update

--- config.php
+++ config.php
@@ -5,4 +5,12 @@ $host = "localhost";
 $user = "root";
 $password = "";
 $dbname = "pi_db"
+
+/*define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_NAME", "pi_db");
+
+
+*/
 ?>
